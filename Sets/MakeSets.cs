8aa3138be556b1794b482d401ba81c781ac78034
Fernickk <fpardo@inetsat.com>
2018-08-06 16:48:15 -0300

    MakeSets script

---
+++ Sets/MakeSets.cs
@@ -0,0 +1,25 @@
+//css_ref System.IO.Compression;
+//css_ref System.IO.Compression.FileSystem;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+class Script
+{
+	[STAThread]
+	static public void Main(string[] args)
+	{
+        DirectoryInfo dirInfo = new DirectoryInfo(".");
+        foreach (var set in dirInfo.GetDirectories())
+        {
+            Console.WriteLine(set.Name);
+            if (File.Exists(set.Name + ".mse-set"))
+            {
+                File.Delete(set.Name + ".mse-set");
+            }
+            ZipFile.CreateFromDirectory(set.Name, set.Name + ".mse-set");
+        }
+	}
+}
+
