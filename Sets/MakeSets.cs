f7b9a3b3d4e5851464a1ff50610774e534435055
Fernickk <fpardo@inetsat.com>
2018-09-13 15:29:04 -0300

    New version of MSE, numbers in symbol

--- Sets/MakeSets.cs
+++ Sets/MakeSets.cs
@@ -8,8 +8,8 @@ using System.Collections.Generic;
 
 class Script
 {
-	static public void Zip()
-	{
+    static public void Zip()
+    {
         DirectoryInfo dirInfo = new DirectoryInfo(".");
         foreach (var set in dirInfo.GetDirectories())
         {
@@ -20,7 +20,7 @@ class Script
             }
             ZipFile.CreateFromDirectory(set.Name, set.Name + ".mse-set");
         }
-	}
+    }
 
     public class Attribute
     {
@@ -48,27 +48,27 @@ class Script
         return "[" + s + "]";
     }
 
-	public class Action
-	{
-		public string Name;
-		public string Type;
-		public string Target;
-		public string Requires;
-		public string AP;
-		public string Text;
-	}
+    public class Action
+    {
+        public string Name;
+        public string Type;
+        public string Target;
+        public string Requires;
+        public string AP;
+        public string Text;
+    }
 
-	public class Minion
-	{
-		public string Name;
-		public string Image;
+    public class Minion
+    {
+        public string Name;
+        public string Image;
         public Dictionary<Attribute, int> Scores = new Dictionary<Attribute, int>();
-		public int Level;
+        public int Level;
         public string SpecialRules = "";
-		public int FirstEntry;
-		public int LastEntry;
+        public int FirstEntry;
+        public int LastEntry;
 
-		public List<Action> Actions = new List<Action>();
+        public List<Action> Actions = new List<Action>();
 
         public string this[Attribute a]
         {
@@ -82,52 +82,58 @@ class Script
         }
     }
 
-	static public void RenderActions(List<Minion> minions)
-	{
+    static public void RenderActions(List<Minion> minions)
+    {
         string timestamp = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd hh:mm:ss");
         if (Directory.Exists("Actions"))
             Directory.Delete("Actions", true);
         Directory.CreateDirectory("Actions");
         using (var writer = new StreamWriter("Actions/set"))
         {
-            writer.WriteLine("mse_version: 2.0.1");
+            writer.WriteLine("mse_version: 0.3.8");
             writer.WriteLine("game: ritualhex");
             writer.WriteLine("stylesheet: action");
-            writer.WriteLine("styling:");
-	        writer.WriteLine("\tritualhex-action:");
-		    writer.WriteLine("\t\tmse_version: 2.0.1");
-		    foreach (var minion in minions)
-		    {
-			    foreach (var action in minion.Actions)
-			    {
+            foreach (var minion in minions)
+            {
+                foreach (var action in minion.Actions)
+                {
                     writer.WriteLine("card:");
                     writer.WriteLine("\thas_styling: false");
-	                writer.WriteLine("\tnotes: ");
-	                writer.WriteLine("\ttime_created: " + timestamp);
-	                writer.WriteLine("\ttime_modified: " + timestamp);
-	                writer.WriteLine("\textra_data:");
-	                writer.WriteLine("\t\tritualhex-action:");
-	                writer.WriteLine("\t\t\tmse_version: 2.0.1");
-	                writer.WriteLine("\t\t\ttarget: \"" + action.Target + "\"");
-	                writer.WriteLine("\t\t\trequires: \"" + action.Requires + "\"");
-	                writer.WriteLine("\tname: \"" + action.Name + "\"");
-	                writer.WriteLine("\timage: local_image_file(\"" + minion.Image + "\")");
-	                writer.WriteLine("\trule_text: \"" + action.Type + "\"");
-	                writer.WriteLine("\tback_text: \"" + action.Text + "\"");
-	                writer.WriteLine("\tfighter: \"" + minion.Name + "\"");
-                    writer.WriteLine("\taction_points: \"" + action.AP + "\"");
+                    writer.WriteLine("\tnotes: ");
+                    writer.WriteLine("\ttime_created: " + timestamp);
+                    writer.WriteLine("\ttime_modified: " + timestamp);
+                    writer.WriteLine("\textra_data:");
+                    writer.WriteLine("\t\tritualhex-action:");
+                    writer.WriteLine("\t\t\ttarget: " + action.Target);
+                    writer.WriteLine("\t\t\trequires: " + action.Requires);
+                    writer.WriteLine("\tname: " + action.Name);
+                    writer.WriteLine("\timage: " + minion.Image);
+                    writer.WriteLine("\trule_text: " + action.Type);
+                    writer.WriteLine("\tback_text:");
+                    using (var textReader = new StringReader(action.Text))
+                    {
+                        while (true)
+                        {
+                            string line = textReader.ReadLine();
+                            if (line == null)
+                                break;
+                            writer.WriteLine("\t\t" + line);
+                        }
+                    }
+                    writer.WriteLine("\tfighter: " + minion.Name);
+                    writer.WriteLine("\taction_points: " + action.AP);
                     File.Copy(Path.Combine("Minions", minion.Image), Path.Combine("Actions", minion.Image), true);
-    		    }
-		    }
+                }
+            }
             writer.WriteLine("version_control:");
-	        writer.WriteLine("\ttype: none");
+            writer.WriteLine("\ttype: none");
             writer.WriteLine("apprentice_code: ");
         }
-	}
+    }
 
     static public string CheckText(object level, object difficulty)
     {
-        return "<b>Check:</b> " + Rand(level) + @" vs. " + difficulty;
+        return "<b>Check:</b> " + Rand(level) + " vs. " + difficulty;
     }
 
     static public string CheckText()
@@ -137,106 +143,106 @@ class Script
 
     static public string AttackText(object attribute, object difficulty, object damage)
     {
-        return CheckText(attribute, difficulty) + @"\n" +
-            "Deal " + damage + " " + Wound + " to the target.";
+        return CheckText(attribute, difficulty) + "\n" +
+            "Deal " + damage + " " + Wound + " to the target.\n\n<sym>56T</sym>: lol";
     }
 
     static public string NegateDefAttackText(object attribute, object difficulty, object damage)
     {
-        return CheckText(attribute, difficulty) + @"\n" +
+        return CheckText(attribute, difficulty) + "\n" +
             "Deal " + damage + " " + Wound + " to the target, ignoring their " + Defense + ".";
     }
 
     static public string HealText(object attribute, object difficulty, object damage)
     {
-        return CheckText(attribute, difficulty) + @"\n" +
+        return CheckText(attribute, difficulty) + "\n" +
             "Remove up to " + damage + " " + Wound + " from the target. They can be removed from multiple body parts.";
     }
 
     static public string GoreText(object attribute, object difficulty, object damage)
     {
-        return AttackText(attribute, difficulty, damage) + @"\n" +
+        return AttackText(attribute, difficulty, damage) + "\n" +
             "Cancel any action the target may have in progress.";
     }
 
     static public string SlapText(object attribute, object difficulty, object damage, object action)
     {
-        return AttackText(attribute, difficulty, damage) + @"\n" +
+        return AttackText(attribute, difficulty, damage) + "\n" +
             "You may follow this action with " + action + ".";
     }
 
     static public string BiteText(object attribute, object difficulty, object damage, object vulnerability)
     {
-        return AttackText(attribute, difficulty, damage) + @"\n" +
+        return AttackText(attribute, difficulty, damage) + "\n" +
             "You may hold the target body part with your head. You may release the hold at any time. Your " + Defense + " and " + Reflexes + " are decreased by " + vulnerability + " while the fighter being held resolves an action.";
     }
 
     static public string DrainAttackText(object attribute, object difficulty, object damage)
     {
-        return AttackText(attribute, difficulty, damage) + @"\n" +
+        return AttackText(attribute, difficulty, damage) + "\n" +
             "You may remove " + Wound + " from yourself, up to the number of " + Wound + " dealt. They can be removed from multiple body parts.";
     }
 
     static public string GnawText(object damage)
     {
-        return "<i>This action can't be interrupted.</i>" + @"\n" +
-            CheckText() + @"\n" +
+        return "<i>This action can't be interrupted.</i>\n" +
+            CheckText() + "\n" +
             "Deal " + damage + " " + Wound + " to the target.";
     }
 
     static public string GrabText(object attribute, object difficulty, object vulnerability)
     {
-        return CheckText(attribute, difficulty) + @"\n" +
+        return CheckText(attribute, difficulty) + "\n" +
             "Hold the target body part with the used member. You may release the hold at any time. Your " + Defense + " and " + Reflexes + " are decreased by " + vulnerability + " while the fighter being held resolves an action.";
     }
 
     static public string PounceText(object attribute, object difficulty, object delay, object action)
     {
-        return CheckText(attribute, difficulty) + @"\n" +
+        return CheckText(attribute, difficulty) + "\n" +
             "Increase the target's " + Time + " by " + delay + ". You may follow this action with " + action + ".";
     }
 
     static public string TeleportText()
     {
-        return CheckText() + @"\n" +
+        return CheckText() + "\n" +
             "Move to an adjacent, unblocked room.";
     }
 
     static public string MoveText(object additionalReflexes)
     {
-        return "<i>While performing this action: add " + additionalReflexes + " to your  " + Reflexes + ". If you receive a  " + Wound + " or one of your body parts is held, cancel this action.</i>" + @"\n" +
+        return "<i>While performing this action: add " + additionalReflexes + " to your  " + Reflexes + ". If you receive a  " + Wound + " or one of your body parts is held, cancel this action.</i>\n" +
             TeleportText();
     }
 
     static public string FlyText(object additionalReflexes)
     {
-        return "<i>Can only be resolved if you have no  " + Wound + " on your wings.</i>" + @"\n" +
+        return "<i>Can only be resolved if you have no  " + Wound + " on your wings.</i>\n" +
             MoveText(additionalReflexes);
     }
 
     static public string WalkText(object additionalReflexes, string affectedPart = "legs")
     {
-        return "<i>Add 1 to the " + Time + " cost for each  " + Wound + " on your " + affectedPart + ".</i>" + @"\n" +
+        return "<i>Add 1 to the " + Time + " cost for each  " + Wound + " on your " + affectedPart + ".</i>\n" +
             MoveText(additionalReflexes);
     }
 
     static public string DefendOtherText(object level, object difficulty)
     {
-        return "<i>Interrupt an attack action targeting a fighter (or body part) other than you (or yours).</i>" + @"\n" +
-            CheckText(level, difficulty) + @"\n" +
+        return "<i>Interrupt an attack action targeting a fighter (or body part) other than you (or yours).</i>\n" +
+            CheckText(level, difficulty) + "\n" +
             "Change the target of the interrupted action to be you (or one of your body parts, if the target was a body part). If the action has more than one target, you can change any number of targets directed to the same fighter.";
     }
 
     static public string CounterspellText(object level, object difficulty)
     {
-        return "<i>Interrupt a magic action.</i>" + @"\n" +
-            CheckText(level, difficulty) + @"\n" +
+        return "<i>Interrupt a magic action.</i>\n" +
+            CheckText(level, difficulty) + "\n" +
             "Increase the difficulty of the interrupted action's check by 1. If the action has no check, it now has a check of " + Rand(Magic) + " vs. 1.";
     }
 
     static public string MindControlText(object level, object difficulty, object turns)
     {
-        return CheckText(level, difficulty) + @"\n" +
+        return CheckText(level, difficulty) + "\n" +
             "Control the target during " + turns + " " + Time + ". Place your Head card as a status marker on the fighter to mark this status. You may cancel this status at any time.";
     }
 
@@ -670,9 +676,9 @@ class Script
         return blighter;
     }
 
-	[STAThread]
-	static public void Main(string[] args)
-	{
+    [STAThread]
+    static public void Main(string[] args)
+    {
         List<Minion> minions = new List<Minion>();
         minions.Add(Imp());
         minions.Add(Thrall());
